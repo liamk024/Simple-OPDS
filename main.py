9ffbfd87dec0cd9c1cdbd8e125c83e36f85561a8
Liam Kelly <liamk024@gmail.com>
2025-09-29 22:08:23 +0100

    begin work on creating atom feed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from flask import Flask, url_for
+from feedgen.feed import FeedGenerator
+from flask import Flask
+
+fg = FeedGenerator()
+fg.id('root')
+fg.title('Simple-OPDS')
+fg.author({'name':'Liam Kelly'})
+
+fe = fg.add_entry()
+fe.id()
 
 app = Flask(__name__)
+
+
