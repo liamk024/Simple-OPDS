a7f197ec1e5ab1dab61b58658d2d6293b9cd3241
Liam Kelly <liamk024@gmail.com>
2025-09-30 20:19:59 +0100

    fix last commit

---
+++ atomgen.py
@@ -0,0 +1,8 @@
+class OPDSFeed():
+    def __init__(self):
+        self.contents = {}
+
+    def __str__(self):
+        xml_declaration = '<?xml version="1.0" encoding="UTF-8"?>'
+        
+        pass
