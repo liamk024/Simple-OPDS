996ccfda9c0a27d97bdb46bdf964e5fbd15937d4
Liam Kelly <liamk024@gmail.com>
2025-10-01 00:00:12 +0100

    construct basic atom feed

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, Response
+import atomgen
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    feed = ''
+    feed = str(atomgen.OPDSFeed())
 
     return Response(feed, mimetype='application/atom+xml')
