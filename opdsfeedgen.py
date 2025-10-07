ea25daa8519038b8a5d5e3dea096062bf11a2c58
Liam Kelly <liamk024@gmail.com>
2025-10-07 18:06:22 +0100

    restructure opdsfeedgen AGAIN

--- opdsfeedgen.py
+++ opdsfeedgen.py
@@ -1,53 +1,57 @@
-import datetime, uuid
+import datetime, os, uuid
+from tomllib import load
+
+config = load(open('opds.toml', 'rb'))
 
 # Generate atom:link element and return as string
 def get_link(rel, href, link_type):
-    out = []
+    link = '<link'
+    link += f' rel="{rel}"'
+    link += f' href="{href}"'
+    link += f' type="{link_type}"'
+    link += '/>'
 
-    out.append('<link')
-    out.append(f'rel="{rel}"')
-    out.append(f'href="{href}"')
-    out.append(f'type="{link_type}"')
+    return link
 
-    out.append('/>')
-
-    return ' '.join(out)
-
-# Main atom feed class, to use as a parent for opds classes
+# Main OPDS feed class
 class OPDSFeed():
-    # Initialise basic data for OPDS Feeds
-    def __init__(self, feed_id, feed_title, is_root=True, feed_type='navigation'):
+    def __init__(self):
         now = datetime.datetime.now(datetime.timezone.utc)
-        self.properties = {
-            'id' : feed_id,
-            'title' : feed_title,
-            'updated' : now.strftime('%d:%m:%YT%H:%M:%S')
-        }
-        
-        if not (feed_type == 'navigation' or feed_type == 'acquisition'):
-            ValueError("Invalid feed_type")
+
+        # Apply config
+        self.base_path = config['base_path']
+        self.title = config['title']
+        self.updated = now.strftime('%d:%m:%YT%H:%M:%S') # TODO: Change to use timestamp for last time files were changed
 
         # Generates the atom:link elements at the top of the feed
         self.links = []
- 
-        if is_root: path = '/' + str(uuid.UUID(self.properties['id']))
-        else: path = '/'
 
-        self.links.append(get_link('self', path, f'application/atom+xml;profile=opds-catalog;kind={feed_type}'))
-        self.links.append(get_link('start', '/', 'application/atom+xml;profile=opds-catalog;kind=navigation'))
+        self.links.append(get_link('self', self.base_path, 'application/atom+xml;profile=opds-catalog;kind=navigation'))
+        self.links.append(get_link('start', self.base_path, 'application/atom+xml;profile=opds-catalog;kind=navigation'))
 
-        self.entries = []
+        # Prepare dictionary for all feed entries
+        self.contents = {}
     
     # For adding OPDS Entries
-    def add_nav_entry(self, entry_id, entry_title):
-        out = []
+    def add_nav_entry(self, title, path, is_directory=True):
+        # out = []
 
-        out.append('    ' + f'<id>{entry_id}</id>')
-        out.append('    ' + f'<title>{entry_title}</title>')
-        out.append('    ' + f'<updated>{self.properties["updated"]}</updated>')
-        out.append('    ' + get_link('subsection', f'/{entry_id}', 'application/atom+xml;profile=opds-catalog;kind=navigation'))
+        # out.append('    ' + f'<id>{entry_id}</id>')
+        # out.append('    ' + f'<title>{entry_title}</title>')
+        # out.append('    ' + f'<updated>{self.properties["updated"]}</updated>')
+        # out.append('    ' + get_link('subsection', f'/{entry_id}', 'application/atom+xml;profile=opds-catalog;kind=navigation'))
 
-        self.entries.append(out)
+        # self.entries.append(out)
+
+        if is_directory:
+            entry_id = "".join([c for c in title.lower() if ord(c) >= 97 and ord(c) <= 122])
+            self.contents[entry_id] = {'title' : title, }
+        else:
+            pass
+
+    # 
+    def return_page(self, page_id):
+        pass
 
     # Properly return document as string to be sent over web
     def __str__(self):
